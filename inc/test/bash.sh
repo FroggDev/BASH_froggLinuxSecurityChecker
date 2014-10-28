4c546e4a24c0263f416f2df28c03eceab5aa5809
Frogg <admin@frogg.fr>
2014-10-28 15:26:33 +0100

    Revert "v1.001"
    This reverts commit af840922d65dc403bf4f164050778206c5078c47.

--- inc/test/bash.sh
+++
@@ -1,9 +0,0 @@
-#===========[BASH]==========
-#version
-v="`bash --version`";vL=($v);v=${vL[3]}
-
-#name							#last version					#current version		#nb test
-DATA[0,0]="bash"				;DATA[0,1]="4.3"				;DATA[0,2]="$v"			;DATA[0,3]=0
-#CVE name						#CVE date						#CVE level				#CVE version list 
-#DATAVUL[0,0,0]=""	;DATAVUL[0,0,1]=""	;DATAVUL[1,0,2]=""	;DATAVUL[1,0,3]=""
-
