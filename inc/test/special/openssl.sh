4b21aaf32a1dd471a958f539872b5b385e25f4a7
Frogg <admin@frogg.fr>
2014-10-29 10:28:39 +0100

    message fix
    poodle vulnerability message was incomplete

--- inc/test/special/openssl.sh
+++ inc/test/special/openssl.sh
@@ -4,7 +4,7 @@
 out="`echo x | timeout 5 ${DATA[$i,0]} s_client -ssl3 -connect ${host}:${port} 2>/dev/null`"
 ret=$?
 case "$ret" in
-0)	scary=1;warn "CVE-2014-3566 (original poodle)" "4.3";;
+0)	scary=1;testko "CVE-2014-3566 (original poodle)" "4.3";;
 124)warn "timeout connecting to host $host:$port\n";;
 1)
 	out=`echo $out | perl -pe 's|.*Cipher is (.*?) .*|$1|'`;
